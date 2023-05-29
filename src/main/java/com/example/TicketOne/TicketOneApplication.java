6345b7afef264aedb3cfc541de8ff304a205d9a4
AltGrEngine <rec01bo20@gmail.com>
2023-05-29 20:44:04 +0200

    spring

---
+++ src/main/java/com/example/TicketOne/TicketOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicketOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TicketOne/TicketOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
